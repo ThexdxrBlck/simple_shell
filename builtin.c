dc41b700e4b5d88aa412aade5c221bfcad2d4b28
TumiKotane <kaiineoyoung@gmail.com>
2023-07-19 01:44:18 -0700

    Updated

--- builtin.c
+++ builtin.c
@@ -13,13 +13,13 @@ int _myexit(info_t *info)
 
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		exitcheck = _erratoi(info->argv[1]);
+		exitcheck = erratoi(info->argv[1]);
 		if (exitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
 			_eputs(info->argv[1]);
-			_eputchar('\n');
+			_putchar('\n');
 			return (1);
 		}
 		info->err_num = _erratoi(info->argv[1]);
